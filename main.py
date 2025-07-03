78871e743c2503bc17773364973ccff83f74d3d8
Basavaraj-ds <akki.basavaraj1997@gmail.com>
2025-07-03 19:33:32 +0530

    complied in main

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from utils.Idata import article, sample_data
+from utils.counter import count_words
+import asyncio
+from utils.translate import translate_text
+
+
 def main():
-    print("Hello from browserstack-solution!")
+    for cnt in range(len(sample_data)):
+        # translate the spanish titles to english
+        sample_data[cnt].eng_title = asyncio.run(translate_text(sample_data[cnt].title))
+
+    # run the counter function to print the words repeated more than twice
+    count_words(sample_data=sample_data)
 
 
 if __name__ == "__main__":
