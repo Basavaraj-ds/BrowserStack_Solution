42c99b5d29c089d1686b0ee117959ec077b2df73
Basavaraj-ds <akki.basavaraj1997@gmail.com>
2025-07-03 19:29:12 +0530

    addded data structure

---
+++ utils/Idata.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class article(BaseModel):
+    title : str
+    body : str
+    eng_title : str = ""
+
+
+# sample data
+sample_data = []
+
+sample_data.append(article(title="El rey está de vacaciones hoy", body=""))
+sample_data.append(article(title="el rey está con la reina", body=""))
+sample_data.append(article(title="el rey fue al palacio", body=""))
+
+
